c246ca2f09725783755c1501336d09be3003f7eb
andre <andre.merlo@dataa.com.br>
2023-10-10 11:32:27 -0300

    cria userRepository

---
+++ src/main/java/br/com/decasamerlo/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.decasamerlo.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    
+}
