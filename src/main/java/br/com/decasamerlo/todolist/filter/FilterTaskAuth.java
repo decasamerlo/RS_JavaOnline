112f44e3a7b24083b06f52965985dcf9e6c3fdbd
andre <andre.merlo@dataa.com.br>
2023-10-15 11:21:54 -0300

    cria mapeamento para update de tarefas

--- src/main/java/br/com/decasamerlo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/decasamerlo/todolist/filter/FilterTaskAuth.java
@@ -24,7 +24,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         var servletPath = request.getServletPath();
-        if ("/tasks/".equals(servletPath)) {
+        if (servletPath.startsWith("/tasks/")) {
             var authorization = request.getHeader("Authorization");
 
             var authEncoded = authorization.substring("Basic".length()).trim();
